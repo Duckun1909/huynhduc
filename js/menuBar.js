f83a9e1d3e0ce5e265ac3e5c912d4130dfedc47f
Pham Huynh Duc <111016470+Duckun1909@users.noreply.github.com>
2022-08-12 01:00:40 +0700

    Update menuBar.js

--- js/menuBar.js
+++ js/menuBar.js
@@ -3,13 +3,13 @@ $(".category-icon").click((e) => {
   console.log(e.target);
   if (checkClick == 1) {
     console.log(1);
-    $(".category-prd").setAttribute(
+    $(".category-prd").attr(
       "style",
       "display: block !important; z-index: 1;"
     );
     checkClick = 0;
   } else {
-    $(".category-prd").setAttribute("style", "display: none !important;");
+    $(".category-prd").attr("style", "display: none !important;");
     checkClick = 1;
   }
 });
